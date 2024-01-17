3112bc94b7886c6d717f2c252914c913bd696261
Matthew Hu <matthewhu39@gmail.com>
2024-01-17 01:44:46 +0000

    Completed prep-javascript-conditionals Exercise.

---
+++ prep-javascript-conditionals/main.js
@@ -0,0 +1,52 @@
+/* Your JS goes in this file */
+const age = 25;
+
+function isAdult(age) {
+  if (age < 18) {
+    return false;
+  } else {
+    return true;
+  }
+}
+console.log('Value of isAdult: ', isAdult(age));
+
+const student1Score = 88;
+
+function didStudentPass(score) {
+  if (score >= 70) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log('Did student pass?', didStudentPass(student1Score));
+
+const student2Score = 102;
+
+function gradeCalculator(score) {
+  if (score < 60) {
+    return 'F';
+  } else if (score < 70) {
+    return 'D';
+  } else if (score < 80) {
+    return 'C';
+  } else if (score < 90) {
+    return 'B';
+  } else if (score < 100) {
+    return 'A';
+  } else {
+    return 'A++';
+  }
+}
+console.log('Grade', gradeCalculator(student2Score));
+
+const dayOfTheWeek = 'thursday';
+
+function dayDetector(dayOfTheWeek) {
+  if (dayOfTheWeek === 'saturday' || dayOfTheWeek === 'sunday') {
+    return 'have a good weekend!';
+  } else {
+    return "It's a weekday!";
+  }
+}
+console.log('dayDetector Result:', dayDetector(dayOfTheWeek));
---
+++ prep-javascript-conditionals/prep-javascript-conditionals-quiz-notes.md
@@ -0,0 +1,45 @@
+# prep-javascript-conditionals-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What does `strictly equal` mean?
+  values must equal in terms of value and type
+
+- What is the logical **and** operator?
+  must satisfy both conditions (&&)
+
+- Can you name some comparison operators?
+  equal to
+  not equal
+  greater than
+  less than or equal to
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
